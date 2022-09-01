6dd66691df5de2eed91c1ae9669afef53b46c1e6
Renard Bergson <renardrock@gmail.com>
2022-09-01 16:35:05 -0300

    adicionando funções onload e onchange

--- js/script.js
+++ js/script.js
@@ -6,6 +6,15 @@ const $currentTime = document.querySelector('.start')
 const $progressBar = document.querySelector('.progressBar')
 const $duration = document.querySelector('.end')
 
+const $volumeBar = document.querySelector('.volumeBar')
+
+// reset function
+document.body.onload = () => {
+    $progressBar.value = 0
+    $volumeBar.value = 100
+}
+
+// play and pause button functions
 $playButton.addEventListener('click', () => {
     $music.play()
 
@@ -20,6 +29,7 @@ $pauseButton.addEventListener('click', () => {
     $pauseButton.style.display = 'none'
 })
 
+// time update and convert functions
 $music.addEventListener('timeupdate', () => {
     $progressBar.value = Math.floor(($music.currentTime / $music.duration) * 100)
 
@@ -36,4 +46,14 @@ $music.addEventListener('timeupdate', () => {
 
     $currentTime.innerHTML = secondsToMinutes( Math.floor($music.currentTime) )
     $duration.innerHTML = secondsToMinutes( Math.floor($music.duration) )
+})
+
+// progress bar change function
+$progressBar.addEventListener('change', () => {
+    $music.currentTime = ($progressBar.value / 100) * $music.duration
+})
+
+// volume change function
+$volumeBar.addEventListener('change', () => {
+    $music.volume = $volumeBar.value / 100
 })
